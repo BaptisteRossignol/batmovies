7c776a3d174b9f01da4a161c7b273918b729afb5
Baptiste Rossignol <baptiste.rossignol@hotmail.fr>
2019-11-28 23:17:34 +0100

    ADD: Movie Filter

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+import 'bootstrap/js/src/tab';
 
 import logoBatmovies from '../img/logo_batmovies.png';
 import anotherPoster from '../img/another_poster.png';
@@ -32,19 +33,22 @@ function myFavourites() {
   document.getElementById('movies-latest').innerHTML = '<p>test</p>';
 }
 
+movieFilter();
+
 latest((results) => {
   const latestMovies = results.results;
   let content = '<h2>Derniers films sortis</h2>';
   const favoriteMovies = Object.keys(sessionStorage);
+
   console.log(favoriteMovies);
 
   latestMovies.forEach((movie) => {
     searchById(movie.id, (oneMovie) => {
-      console.log(favoriteMovies.includes(oneMovie.id));
+      console.log(oneMovie.vote_average * 10);
       const date = new Date(oneMovie.release_date);
       const dateMovie = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 
-      content += '<div class="col-md-8"><div class="movie">';
+      content += '<div class="movie">';
 
       if (oneMovie.poster_path == null) {
         content += `<img src="dist/${anotherPoster}" alt="Aucune affiche disponible pour ce film">`;
@@ -57,12 +61,12 @@ latest((results) => {
                       <p class="movie-title">${oneMovie.title}</p>`;
 
       if (favoriteMovies.includes(oneMovie.id.toString())) {
-        content += `<div id="${oneMovie.id}" onClick="sessionStorage.removeItem(id)">
-                      <i style="color: red;" class="fas fa-heart"></i>
+        content += `<div id="${oneMovie.id}" class="favorite" onClick="sessionStorage.removeItem(id)">
+                      <i style="color: red;"  class="fas fa-heart favorite"></i>
                     </div>`;
       } else {
-        content += `<div id="${oneMovie.id}" onClick="sessionStorage.setItem(id, id)">
-                      <i style="color: red;" class="far fa-heart"></i>
+        content += `<div id="${oneMovie.id}" class="favorite" onClick="sessionStorage.setItem(id, id)">
+                      <i style="color: red;" class="far fa-heart favorite"></i>
                     </div>`;
       }
 
@@ -72,8 +76,13 @@ latest((results) => {
       if (oneMovie.runtime === 0 || oneMovie.runtime == null) {
         content += '</p>';
       } else {
-        content += ` | ${timeConvert(oneMovie.runtime)}</p>`;
+        content += ` | ${timeConvert(oneMovie.runtime)}`;
       }
+
+      if(oneMovie.vote_average != 0) {
+        content += ` | <div class="star-rating" style="width: ${oneMovie.vote_average * 10}%"></div>`;
+      }
+
 
       if (oneMovie.overview === '') {
         content += "<p class='movie-overview'>Aucune description n'est disponible pour ce film.</p>";
@@ -81,7 +90,7 @@ latest((results) => {
         content += `<p class="movie-overview">${oneMovie.overview}</p>`;
       }
 
-      content += '</div></div>';
+      content += '</div>';
       content += '</div>';
 
       document.getElementById('movies-latest').innerHTML = content;
@@ -89,6 +98,41 @@ latest((results) => {
   });
 });
 
-document.getElementById(
-  'logo_batmovies',
-).innerHTML = `<img src="dist/${logoBatmovies}" alt="Logo BatMovies">`;
+function movieFilter() {
+  let content = '<div class="movies-filter">';
+
+  content += `<nav>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Filtrer</a>
+    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Trier</a>
+  </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Filtrer</div>
+  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">Trier</div>
+</div>`;
+  content += '</div>';
+  document.getElementById('movies-filter').innerHTML = content;
+}
+
+document.getElementById('logo_batmovies').innerHTML = `<img src="dist/${logoBatmovies}" alt="Logo BatMovies">`;
+
+function changeHeart(id) {
+  const favoriteMovies = Object.keys(sessionStorage);
+  const element = document.getElementById(id).childNodes;
+  console.log(element);
+
+  if (favoriteMovies.includes(id.toString())) {
+    element.className.remove('fas');
+    element.className.add('far');
+  } else {
+    element.className.remove('far');
+    element.className.add('fas');
+  }
+}
+
+document.getElementsByClassName('favorite').onclick = () => {
+  const fav = this.getAttribute('id');
+  console.log(fav);
+  changeHeart(fav);
+};
